f12afd2d29c8c06242b2c9293958ae2434e2c5bb
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2026-02-10 09:17:57 +0100

    feat: #ENABLING-587, improve device detection for stats

--- src/ts/utils/index.ts
+++ src/ts/utils/index.ts
@@ -5,4 +5,5 @@ export * from "./localization";
 export * from "./TrackedScope";
 export * from './browser-info';
 export * from './base64';
+export * from './deviceDetection';
 export { KnownWidget as TrackedActionFromWidget } from '../modules/widgets.module';
